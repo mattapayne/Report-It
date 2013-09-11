2c9df691aa4520211c187a42ef24dbd9dde84300
Matt Payne <paynmatt@gmail.com>
2013-09-10 22:23:38 -0700

    Remove some console.logs

--- config/dataStoreConfig.js
+++ config/dataStoreConfig.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 exports.init = function() {
-  console.log("*****************************");
   var config = InspectIt.app.get('config');
-  console.log(InspectIt.app.get('config'));
   mongoose.connect(config.mongodb);
 }
