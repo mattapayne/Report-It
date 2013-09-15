ee055570f7788c93d6e8824f2b5df097c5c54b78
Matt Payne <paynmatt@gmail.com>
2013-09-15 11:28:48 -0700

    Switch to a library for better image handling

--- controllers/images.js
+++ controllers/images.js
@@ -1,9 +1,85 @@
 var aws = require('aws-sdk'),
     uuid = require('node-uuid'),
-    mkdirp = require('mkdirp'),
-    fs = require('fs');
-    
-var AWS_URI = 'https://s3.amazonaws.com';
+    _ = require('underscore'),
+    uploadfs = require('uploadfs')();
+
+var IMAGE_HEIGHT_KEY = 'image_height';
+var DEFAULT_IMAGE_HEIGHT = 360;
+var IMAGE_WIDTH_KEY = 'image_width';
+var DEFAULT_IMAGE_WIDTH = 640;
+
+function getLocalConfigurationOptions(imageSizes) {
+  return {
+    backend: 'local',
+    uploadsPath: ReportIt.rootDirectory + '/public/uploads',
+    uploadsUrl: 'http://localhost:' + ReportIt.app.get('port') + '/uploads',
+    tempPath: ReportIt.rootDirectory + '/temp',
+    imageSizes: imageSizes,
+    parallel: 1
+  };
+}
+
+function getS3ConfigurationOptions(args) {
+  var config = ReportIt.app.get('config').aws;
+  return {
+    backend: 's3',
+    secret: config.secret_access_key,
+    key: config.access_key_id,
+    bucket: config.bucket_name,
+    region: config.region,
+    tempPath: ReportIt.rootDirectory + '/temp',
+    imageSizes: imageSizes,
+    parallel: 1
+  };
+}
+
+function getUploadOptions(user) {
+  var height = _.find(user.settings, function(s) {
+    return s.key === IMAGE_HEIGHT_KEY;
+  });
+  
+  var width = _.find(user.settings, function(s) {
+    return s.key === IMAGE_WIDTH_KEY;
+  });
+  
+  var imageSizes = [
+    {
+      name: 'resized',
+      height: (height && height.value) ? height.value : DEFAULT_IMAGE_HEIGHT,
+      width: (width && width.value) ? width.value : DEFAULT_IMAGE_WIDTH
+    }
+  ];
+  
+  return ReportIt.app.get('env') === 'development' ?
+    getLocalConfigurationOptions(imageSizes) :
+    getS3ConfigurationOptions(imageSizes);
+
+}
+
+exports.upload = function(req, res) {
+  var options = getUploadOptions(req.user);
+  uploadfs.init(options, function(err) {
+    if(err) {
+      console.log(err);
+      res.send(500)
+    }
+    else {
+      uploadfs.copyImageIn(req.files.file.path, '/'+ uuid.v4(), function(err, info) {
+          if (err) {
+            console.log(err);
+            res.send(500);
+          }
+          else {
+            res.send({
+              filelink: uploadfs.getUrl() + info.basePath + '.resized' + info.extension
+            });
+          }
+      });
+    }
+  });
+  
+}
+/*var AWS_URI = 'https://s3.amazonaws.com';
     
 function saveFile(res, s3, destination_bucket, fileName, data) {
   s3.putObject({
@@ -75,4 +151,4 @@ exports.upload = function(req, res) {
         }
       }
   });
-}
+}*/
