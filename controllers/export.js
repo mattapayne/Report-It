f242d3702129f7802635f06cb2c3c5a5d98ff833
mattapayne <paynmatt@gmail.com>
2013-09-18 20:01:02 -0700

    Initial export to pdf

--- controllers/export.js
+++ controllers/export.js
@@ -1,17 +1,23 @@
-var errorParser = require('../utility/mongoose_error_parser.js');
-var Report = ReportIt.models('Report');
-var ReportTemplate = ReportIt.models('ReportTemplate');
-var mime = require('mime');
-var fs = require('fs');
+var errorParser = require('../utility/mongoose_error_parser.js'),
+    Report = ReportIt.models('Report'),
+    ReportTemplate = ReportIt.models('ReportTemplate'),
+    mime = require('mime'),
+    wkhtmltopdf = require('wkhtmltopdf'),
+    fs = require('fs');
 
 function sendFile(req, res, format, item) {
   var content = item.content;
   var ext = format === 'word' ? ".doc" : ".pdf";
-  var name = item.name + ext;
-  var mimeType = mime.lookup(name);
-  res.setHeader('Content-Disposition', 'attachment; filename=' + name);
-  res.setHeader('Content-Type', mimeType);
-  res.send(content);
+  if (ext == '.doc') {
+    var name = item.name + ext;
+    var mimeType = mime.lookup(name);
+    res.setHeader('Content-Disposition', 'attachment; filename=' + name);
+    res.setHeader('Content-Type', mimeType);
+    res.send(content);  
+  }
+  else {
+    wkhtmltopdf(item.content).pipe(res);
+  }
 }
 
 exports.word = function(req, res) {
